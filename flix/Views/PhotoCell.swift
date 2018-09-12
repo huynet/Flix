a7b48fd9f3c02213d45191a19968717bd756b904
Huy Pham <pharvard@Huys-MacBook-Pro.local>
2018-09-12 16:36:47 -0500

    superhero view

---
+++ flix/Views/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  flix
+//
+//  Created by Huy Pham on 9/12/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Huy Pham on 9/12/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
