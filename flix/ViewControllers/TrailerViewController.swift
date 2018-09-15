4729217256591edbe7e85c6daa588b3b6ed8953d
Huy Pham <pharvard@Huys-MacBook-Pro.local>
2018-09-14 21:55:30 -0500

    clean up before destroy

--- flix/ViewControllers/TrailerViewController.swift
+++ flix/ViewControllers/TrailerViewController.swift
@@ -10,12 +10,10 @@ import UIKit
 import WebKit
 
 class TrailerViewController: UIViewController, WKUIDelegate {
-    
+
     @IBOutlet weak var webView: WKWebView!
     
     var movie: [String: Any]?
-    
-    //var key: String?
     
     var temp: String?
     
@@ -32,12 +30,9 @@ class TrailerViewController: UIViewController, WKUIDelegate {
         if let movie = movie {
             let idOptional = movie["id"]
             if let id = idOptional {
-                print("ID is \(id)")
                 let url = URL(string: "https://api.themoviedb.org/3/movie/\(id)/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US")!
-                print(url)
                 let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
                 let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-                var key = "test"
                 let task = session.dataTask(with: request) { (data, response, error) in
                     if let error = error {
                         print(error.localizedDescription)
@@ -45,9 +40,8 @@ class TrailerViewController: UIViewController, WKUIDelegate {
                         let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                         let movies = dataDictionary["results"] as! [[String: Any]]
                         
-                        let movie = movies[0]["key"] as! String
-                        let key1 = movie
-                        let baseURLString = "https://www.youtube.com/watch?v=\(String(describing: key1))"
+                        let key = movies[0]["key"] as! String
+                        let baseURLString = "https://www.youtube.com/watch?v=\(String(describing: key))"
                         let myURL = URL(string: baseURLString)
                         let myRequest = URLRequest(url: myURL!)
                         self.webView.load(myRequest)
@@ -62,24 +56,4 @@ class TrailerViewController: UIViewController, WKUIDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func etchVideos(id: Int) {
-        let url = URL(string: "https://api.themoviedb.org/3/movie/\(id)/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        var key: String?
-        let task = session.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                let movies = dataDictionary["results"] as! [[String: Any]]
-                
-                let movie = movies[0]["key"] as! String
-                self.temp = movie
-            }
-        }
-        task.resume()
-    }
-    
 }
